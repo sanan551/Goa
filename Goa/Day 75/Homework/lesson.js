a682f80ef40c9a5158702cc9d18143ed1a04b07f
სანან მამედოვი <mamedovisanan57@gmail.com>
2025-03-06 19:38:14 +0400

    Add files via upload

---
+++ Goa/Day 75/Homework/lesson.js
@@ -0,0 +1,18 @@
+
+function printNumbers() {
+    let num1 = 15;
+    let num2 = 7;
+    console.log(num1 + num2);
+    console.log(num1 - num2);
+    console.log(num1 * num2);
+    console.log( num1 / num2);
+};
+
+
+function modifyButton() {
+    let button = document.getElementById("myButton");
+    button.style.color = "white";
+    button.style.backgroundColor = "blue";
+    button.style.borderRadius = "10px";
+    button.style.width = "150px";
+}
