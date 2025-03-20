6890109b577d5ecc8b0156e79649c664def6cac9
სანან მამედოვი <mamedovisanan57@gmail.com>
2025-03-20 20:28:31 +0400

    Add files via upload

---
+++ Goa/Day 79/Classwork/lesson.js
@@ -0,0 +1,8 @@
+let userName = prompt("გთხოვთ, შეიყვანოთ თქვენი სახელი:");
+alert("მოგესალმებით, " + userName);
+
+let Confirmed = confirm("გნებავთ, რომ გააგრძელოთ?");
+
+console.log("მომხმარებლის სახელი: " + userName);
+console.log("მომხმარებლის არჩევანი: " + Confirmed);
+
