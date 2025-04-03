b1e1b0762181e3a105a3b9848b0882ba8183b3f7
სანან მამედოვი <mamedovisanan57@gmail.com>
2025-04-03 19:52:01 +0400

    Add files via upload

---
+++ Goa/Day 83/Classwork/lesson.js
@@ -0,0 +1,17 @@
+function logVariableTypes() {
+    let a = 5; 
+    let b = "Hello"; 
+    let c = true; 
+    console.log(typeof a);
+    console.log(typeof b);
+    console.log(typeof c);
+}
+logVariableTypes();
+
+
+// Sequencing - კოდი სრულდება ზედიზედ, ნაბიჯ-ნაბიჯ, ზუსტად იმ რიგით, როგორც დაწერილია.
+
+// Iteration  – კოდის ნაწილი მრავალჯერ სრულდება, სანამ პირობა მართალია.
+
+// Selection  – კოდი ირჩევს, რომელი ნაწილი შესრულდეს კონკრეტული პირობის მიხედვით.
+
