5433f30f03d0e840a5e174c3bc83c9c60d91b605
სანან მამედოვი <mamedovisanan57@gmail.com>
2025-05-29 20:53:41 +0400

    Add files via upload

---
+++ Goa/Day 98/Classwork/lesson.js
@@ -0,0 +1,30 @@
+// function Film (name, genre, year , imdb , review) {
+//     this.name = name
+//     this.genre = genre
+//     this.year = year
+//     this.imdb = imdb
+//     this.review = review
+// }
+
+// const film1 = new Film("The Matrix","Action", 1999 , 8.7, "When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.")
+
+// console.log(film1.name, film1.genre , film1.year, film1.imdb, film1.review)
+
+
+function Person(name,lastname,age) {
+    this.name = name;
+    this.lastname = lastname;
+    this.age = age;
+}
+
+const person1 = new Person("Sanan"," Mamedov",15 )
+
+function Button1 () {
+    console.log(person1)
+}
+
+
+
+
+
+  
